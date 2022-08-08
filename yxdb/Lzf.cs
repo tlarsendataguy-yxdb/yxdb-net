c47be96278460561d2a7bf65e95dc8400a9400b7
Tom Larsen <larsenthomasj@gmail.com>
2022-08-07 20:09:12 -0400

    start with Lzf

---
+++ yxdb/Lzf.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("yxdb_test")]
+namespace yxdb
+{
+    internal class Lzf
+    {
+        public Lzf(byte[] inBuffer, byte[] outBuffer)
+        {
+            _inBuffer = inBuffer;
+            _outBuffer = outBuffer;
+        }
+
+        private readonly byte[] _inBuffer;
+        private readonly byte[] _outBuffer;
+        private int _iidx;
+        private int _oidx;
+        private int _inLen;
+
+        public int Decompress(int len)
+        {
+            _inLen = len;
+            Reset();
+
+            if (_inLen == 0)
+            {
+                return 0;
+            }
+
+            while (_iidx < _inLen)
+            {
+                var ctrl = _inBuffer[_iidx];
+                _iidx++;
+
+                if (ctrl < 32)
+                {
+                    CopyByteSequence(ctrl);
+                }
+                else
+                {
+                    ExpandRepeatedBytes(ctrl);
+                }
+            }
+
+            return _oidx;
+        }
+
+        private void Reset()
+        {
+            _iidx = 0;
+            _oidx = 0;
+        }
+
+        private void CopyByteSequence(byte ctrl)
+        {
+            var len = ctrl + 1;
+            if (_oidx + len > _outBuffer.Length)
+            {
+                throw new ArgumentException("output array is too small");
+            }
+            Array.Copy(_inBuffer, _iidx, _outBuffer, _oidx, len);
+            _oidx += len;
+            _iidx += len;
+        }
+
+        private void ExpandRepeatedBytes(byte ctrl)
+        {
+            var length = ctrl >> 5;
+            var reference = _oidx - ((ctrl & 0x1f) << 8) - 1;
+
+            if (length == 7)
+            {
+                length += _inBuffer[_iidx];
+                _iidx++;
+            }
+
+            if (_oidx + length + 2 > _outBuffer.Length)
+            {
+                throw new ArgumentException("output array is too small");
+            }
+
+            reference -= _inBuffer[_iidx];
+            _iidx++;
+
+            length += 2;
+
+            while (length > 0)
+            {
+                var size = Math.Min(length, _oidx - reference);
+                reference = CopyFromReferenceAndIncrement(reference, size);
+                length -= size;
+            }
+        }
+
+        private int CopyFromReferenceAndIncrement(int reference, int size)
+        {
+            Array.Copy(_outBuffer, reference, _outBuffer, _oidx, size);
+            _oidx += size;
+            return reference + size;
+        }
+    }
+}
---
+++ yxdb_test/LzfTest.cs
@@ -0,0 +1,108 @@
+using System;
+using Xunit;
+using Xunit.Sdk;
+using yxdb;
+
+namespace yxdb_test
+{
+    public class LzfTest
+    {
+        [Fact]
+        public void TestEmptyInput()
+        {
+            var inData = new byte[]{};
+            var outData = new byte[]{};
+            var lzf = new Lzf(inData, outData);
+
+            var written = lzf.Decompress(0);
+            Assert.Equal(0, written);
+        }
+
+        [Fact]
+        public void OutputArrayIsTooSmall()
+        {
+            var inData = new byte[] { 0, 25 };
+            var outData = new byte[] { };
+            var lzf = new Lzf(inData, outData);
+            
+            Assert.Throws<ArgumentException>(()=> lzf.Decompress(2));
+        }
+
+        [Fact]
+        public void SmallControlValuesDoSimpleCopies()
+        {
+            var inData = new byte[] { 4, 1, 2, 3, 4, 5 };
+            var outData = new byte[5];
+            var lzf = new Lzf(inData, outData);
+
+            var written = lzf.Decompress(6);
+            Assert.Equal(5, written);
+            Assert.Equal(new byte[]{1,2,3,4,5}, outData);
+        }
+
+        [Fact]
+        public void MultipleSmallControlValues()
+        {
+            var inData = new byte[] { 2, 1, 2, 3, 1, 1, 2 };
+            var outData = new byte[5];
+            var lzf = new Lzf(inData, outData);
+
+            var written = lzf.Decompress(7);
+            Assert.Equal(5, written);
+            Assert.Equal(new byte[]{1,2,3,1,2}, outData);
+        }
+
+        [Fact]
+        public void ExpandLargeControlValues()
+        {
+            var inData = new byte[] { 2, 1, 2, 3, 32, 1 };
+            var outData = new byte[6];
+            var lzf = new Lzf(inData, outData);
+
+            var written = lzf.Decompress(6);
+            Assert.Equal(6, written);
+            Assert.Equal(new byte[]{1,2,3,2,3,2}, outData);
+        }
+
+        [Fact]
+        public void LargeControlValuesWithLengthOf7()
+        {
+            var inData = new byte[] { 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 224, 1, 8 };
+            var outData = new byte[19];
+            var lzf = new Lzf(inData, outData);
+
+            var written = lzf.Decompress(13);
+            Assert.Equal(19, written);
+            Assert.Equal(new byte[]{1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1}, outData);
+        }
+
+        [Fact]
+        public void OutputArrayTooSmallForLargeControlValues()
+        {
+            var inData = new byte[]{8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 224, 1, 8};
+            var outData = new byte[17];
+            var lzf = new Lzf(inData, outData);
+
+            Assert.Throws<ArgumentException>(() =>lzf.Decompress(13));
+        }
+
+        [Fact]
+        public void ResetLzfAndStartAgain()
+        {
+            var inData = new byte[]{4, 1, 2, 3, 4, 5};
+            var outData = new byte[5];
+            var lzf = new Lzf(inData, outData);
+
+            lzf.Decompress(6);
+
+            inData[0] = 2;
+            inData[1] = 6;
+            inData[2] = 7;
+            inData[3] = 8;
+
+            var written = lzf.Decompress(4);
+            Assert.Equal(3, written);
+            Assert.Equal(new byte[]{6, 7, 8, 4, 5}, outData);
+        }
+    }
+}
