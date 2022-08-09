7e7d5d52739b26bec07dfe7c91305febab3a0d15
Tom Larsen <larsenthomasj@gmail.com>
2022-08-09 06:05:08 -0400

    add yxdbrecord

--- yxdb/YxdbRecord.cs
+++ yxdb/YxdbRecord.cs
@@ -1,7 +1,254 @@
+using System;
+using System.Collections.Generic;
+
 namespace yxdb
 {
     internal class YxdbRecord
     {
+        private YxdbRecord(int fieldCount)
+        {
+            _nameToIndex = new Dictionary<string, int>(fieldCount);
+            Fields = new List<YxdbField>(fieldCount);
+            _boolExtractors = new Dictionary<int, Func<byte[], bool?>>();
+            _byteExtractors = new Dictionary<int, Func<byte[], byte?>>();
+            _longExtractors = new Dictionary<int, Func<byte[], long?>>();
+            _doubleExtractors = new Dictionary<int, Func<byte[], double?>>();
+            _stringExtractors = new Dictionary<int, Func<byte[], string>>();
+            _dateExtractors = new Dictionary<int, Func<byte[], DateTime?>>();
+            _blobExtractors = new Dictionary<int, Func<byte[], byte[]>>();
+            FixedSize = 0;
+            HasVar = false;
+        }
+
+        public readonly List<YxdbField> Fields;
+        public int FixedSize;
+        public bool HasVar;
+        private readonly Dictionary<string, int> _nameToIndex;
+        private readonly Dictionary<int, Func<byte[], bool?>> _boolExtractors;
+        private readonly Dictionary<int, Func<byte[], byte?>> _byteExtractors;
+        private readonly Dictionary<int, Func<byte[], long?>> _longExtractors;
+        private readonly Dictionary<int, Func<byte[], double?>> _doubleExtractors;
+        private readonly Dictionary<int, Func<byte[], string>> _stringExtractors;
+        private readonly Dictionary<int, Func<byte[], DateTime?>> _dateExtractors;
+        private readonly Dictionary<int, Func<byte[], byte[]>> _blobExtractors;
+
+        public static YxdbRecord FromFieldList(List<MetaInfoField> fields)
+        {
+            var record = new YxdbRecord(fields.Count);
+            var startAt = 0;
+            foreach (var field in fields)
+            {
+                int size;
+                switch (field.Type)
+                {
+                    case "Int16":
+                        record.AddLongExtractor(field.Name, Extractors.NewInt16Extractor(startAt));
+                        startAt += 3;
+                        break;
+                    case "Int32":
+                        record.AddLongExtractor(field.Name, Extractors.NewInt32Extractor(startAt));
+                        startAt += 5;
+                        break;
+                    case "Int64":
+                        record.AddLongExtractor(field.Name, Extractors.NewInt64Extractor(startAt));
+                        startAt += 9;
+                        break;
+                    case "Float":
+                        record.AddDoubleExtractor(field.Name, Extractors.NewFloatExtractor(startAt));
+                        startAt += 5;
+                        break;
+                    case "Double":
+                        record.AddDoubleExtractor(field.Name, Extractors.NewDoubleExtractor(startAt));
+                        startAt += 9;
+                        break;
+                    case "FixedDecimal":
+                        size = field.Size;
+                        record.AddDoubleExtractor(field.Name, Extractors.NewFixedDecimalExtractor(startAt, size));
+                        startAt += size + 1;
+                        break;
+                    case "String":
+                        size = field.Size;
+                        record.AddStringExtractor(field.Name, Extractors.NewStringExtractor(startAt, size));
+                        startAt += size + 1;
+                        break;
+                    case "WString":
+                        size = field.Size;
+                        record.AddStringExtractor(field.Name, Extractors.NewWStringExtractor(startAt, size));
+                        startAt += (size * 2) + 1;
+                        break;
+                    case "V_String":
+                        record.AddStringExtractor(field.Name, Extractors.NewV_StringExtractor(startAt));
+                        startAt += 4;
+                        record.HasVar = true;
+                        break;
+                    case "V_WString":
+                        record.AddStringExtractor(field.Name, Extractors.NewV_WStringExtractor(startAt));
+                        startAt += 4;
+                        record.HasVar = true;
+                        break;
+                    case "Date":
+                        record.AddDateExtractor(field.Name, Extractors.NewDateExtractor(startAt));
+                        startAt += 11;
+                        break;
+                    case "DateTime":
+                        record.AddDateExtractor(field.Name, Extractors.NewDateTimeExtractor(startAt));
+                        startAt += 20;
+                        break;
+                    case "Bool":
+                        record.AddBoolExtractor(field.Name, Extractors.NewBoolExtractor(startAt));
+                        startAt += 1;
+                        break;
+                    case "Byte":
+                        record.AddByteExtractor(field.Name, Extractors.NewByteExtractor(startAt));
+                        startAt += 2;
+                        break;
+                    case "Blob":
+                    case "SpatialObj":
+                        record.AddBlobExtractor(field.Name, Extractors.NewBlobExtractor(startAt));
+                        startAt += 4;
+                        record.HasVar = true;
+                        break;
+                    default:
+                        throw new ArgumentException("field type is not supported; yxdb metadata is not valid");
+                }
+            }
+
+            record.FixedSize = startAt;
+            return record;
+        }
+
+        public long? ExtractLongFrom(int index, byte[] buffer)
+        {
+            var extractor = _longExtractors[index];
+            return extractor(buffer);
+        }
+
+        public long? ExtractLongFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractLongFrom(index, buffer);
+        }
+
+        public double? ExtractDoubleFrom(int index, byte[] buffer)
+        {
+            var extractor = _doubleExtractors[index];
+            return extractor(buffer);
+        }
+
+        public double? ExtractDoubleFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractDoubleFrom(index, buffer);
+        }
+
+        public string ExtractStringFrom(int index, byte[] buffer)
+        {
+            var extractor = _stringExtractors[index];
+            return extractor(buffer);
+        }
+
+        public string ExtractStringFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractStringFrom(index, buffer);
+        }
+
+        public DateTime? ExtractDateFrom(int index, byte[] buffer)
+        {
+            var extractor = _dateExtractors[index];
+            return extractor(buffer);
+        }
+
+        public DateTime? ExtractDateFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractDateFrom(index, buffer);
+        }
+
+        public bool? ExtractBoolFrom(int index, byte[] buffer)
+        {
+            var extractor = _boolExtractors[index];
+            return extractor(buffer);
+        }
+
+        public bool? ExtractBoolFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractBoolFrom(index, buffer);
+        }
+
+        public byte? ExtractByteFrom(int index, byte[] buffer)
+        {
+            var extractor = _byteExtractors[index];
+            return extractor(buffer);
+        }
+
+        public byte? ExtractByteFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractByteFrom(index, buffer);
+        }
+
+        public byte[] ExtractBlobFrom(int index, byte[] buffer)
+        {
+            var extractor = _blobExtractors[index];
+            return extractor(buffer);
+        }
+
+        public byte[] ExtractBlobFrom(string name, byte[] buffer)
+        {
+            var index = _nameToIndex[name];
+            return ExtractBlobFrom(index, buffer);
+        }
+
+        private void AddLongExtractor(string name, Func<byte[], long?> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Long);
+            _longExtractors[index] = extractor;
+        }
+
+        private void AddDoubleExtractor(string name, Func<byte[], double?> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Double);
+            _doubleExtractors[index] = extractor;
+        }
+
+        private void AddStringExtractor(string name, Func<byte[], string> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.String);
+            _stringExtractors[index] = extractor;
+        }
         
+        private void AddDateExtractor(string name, Func<byte[], DateTime?> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Date);
+            _dateExtractors[index] = extractor;
+        }
+
+        private void AddBoolExtractor(string name, Func<byte[], bool?> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Boolean);
+            _boolExtractors[index] = extractor;
+        }
+
+        private void AddByteExtractor(string name, Func<byte[], byte?> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Byte);
+            _byteExtractors[index] = extractor;
+        }
+
+        private void AddBlobExtractor(string name, Func<byte[], byte[]> extractor)
+        {
+            var index = AddFieldNameToIndexMap(name, YxdbField.DataType.Blob);
+            _blobExtractors[index] = extractor;
+        }
+
+        private int AddFieldNameToIndexMap(string name, YxdbField.DataType type)
+        {
+            var index = Fields.Count;
+            Fields.Add(new YxdbField(name, type));
+            _nameToIndex[name] = index;
+            return index;
+        }
     }
 }
--- yxdb_test/YxdbRecordTest.cs
+++ yxdb_test/YxdbRecordTest.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
+using yxdb;
 
 namespace yxdb_test
 {
@@ -7,7 +10,204 @@ namespace yxdb_test
         [Fact]
         public void TestReadInt16Record()
         {
+            var record = LoadRecordWithValueColumn("Int16", 2);
+            var source = new byte[] { 23, 0, 0 };
             
+            Assert.Equal(1, record.Fields.Count);
+            Assert.Equal("value", record.Fields[0].Name);
+            Assert.Equal(YxdbField.DataType.Long, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(3, record.FixedSize);
+            Assert.Equal(23, record.ExtractLongFrom(0, source));
+            Assert.Equal(23, record.ExtractLongFrom("value", source));
+        }
+
+        [Fact]
+        public void TestReadInt32Record()
+        {
+            var record = LoadRecordWithValueColumn("Int32", 4);
+            var source = new byte[] { 23, 0, 0, 0, 0};
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(5, record.FixedSize);
+            Assert.Equal(23, record.ExtractLongFrom(0, source));
+        }
+
+        [Fact]
+        public void TestReadInt64Record()
+        {
+            var record = LoadRecordWithValueColumn("Int64", 8);
+            var source = new byte[] { 23, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(9, record.FixedSize);
+            Assert.Equal(23, record.ExtractLongFrom(0, source));
+        }
+
+        [Fact]
+        public void TestReadFloatRecord()
+        {
+            var record = LoadRecordWithValueColumn("Float", 4);
+            var source = new byte[] { 205, 206, 140, 63, 0 };
+            var expected = BitConverter.ToSingle(new byte[] { 205, 206, 140, 63 }); 
+            
+            Assert.Equal(YxdbField.DataType.Double, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(5, record.FixedSize);
+            Assert.Equal(expected, record.ExtractDoubleFrom(0, source));
+            Assert.Equal(expected, record.ExtractDoubleFrom("value", source));
+        }
+
+        [Fact] public void TestReadDoubleRecord()
+        {
+            var record = LoadRecordWithValueColumn("Double", 8);
+            var source = new byte[] { 154, 155, 155, 155, 155, 155, 241, 63, 0 };
+            var expected = BitConverter.ToDouble(new byte[] { 154, 155, 155, 155, 155, 155, 241, 63 }); 
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(9, record.FixedSize);
+            Assert.Equal(expected, record.ExtractDoubleFrom(0, source));
+        }
+
+        [Fact] public void TestReadFixedDecimalRecord()
+        {
+            var record = LoadRecordWithValueColumn("FixedDecimal", 10);
+            var source = new byte[] { 49, 50, 51, 46, 52, 53, 0, 43, 67, 110, 0 };
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(11, record.FixedSize);
+            Assert.Equal(123.45, record.ExtractDoubleFrom(0, source));
+        }
+        
+        [Fact]
+        public void TestReadStringRecord()
+        {
+            var record = LoadRecordWithValueColumn("String", 15);
+            var source = new byte[] { 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 0, 23, 77, 0 };
+            
+            Assert.Equal(YxdbField.DataType.String, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(16, record.FixedSize);
+            Assert.Equal("hello world!", record.ExtractStringFrom(0, source));
+            Assert.Equal("hello world!", record.ExtractStringFrom("value", source));
+        }
+        
+        [Fact]
+        public void TestReadWStringRecord()
+        {
+            var record = LoadRecordWithValueColumn("WString", 15);
+            var source = new byte[] { 104, 0, 101, 0, 108, 0, 108, 0, 111, 0, 32, 0, 119, 0, 111, 0, 114, 0, 108, 0, 100, 0, 33, 0, 0, 0, 23, 0, 77, 0, 0 };
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(31, record.FixedSize);
+            Assert.Equal("hello world!", record.ExtractStringFrom(0, source));
+        }
+        
+        [Fact]
+        public void TestReadV_StringRecord()
+        {
+            var record = LoadRecordWithValueColumn("V_String", 15);
+            var source = new byte[] { 0, 0, 0, 0, 4, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            
+            Assert.Equal(true, record.HasVar);
+            Assert.Equal(4, record.FixedSize);
+            Assert.Equal("", record.ExtractStringFrom(0, source));
+        }
+        
+        [Fact]
+        public void TestReadV_WStringRecord()
+        {
+            var record = LoadRecordWithValueColumn("V_WString", 15);
+            var source = new byte[] { 0, 0, 0, 0, 4, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            
+            Assert.Equal(true, record.HasVar);
+            Assert.Equal(4, record.FixedSize);
+            Assert.Equal("", record.ExtractStringFrom(0, source));
+        }
+                
+        [Fact]
+        public void TestReadDateRecord()
+        {
+            var record = LoadRecordWithValueColumn("Date", 10);
+            var source = new byte[] { 50, 48, 50, 49, 45, 48, 49, 45, 48, 49, 0 };
+            var expected = new DateTime(2021, 1, 1);
+            
+            Assert.Equal(YxdbField.DataType.Date, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(11, record.FixedSize);
+            Assert.Equal(expected, record.ExtractDateFrom(0, source));
+            Assert.Equal(expected, record.ExtractDateFrom("value", source));
+        }
+                
+        [Fact]
+        public void TestReadDateTimeRecord()
+        {
+            var record = LoadRecordWithValueColumn("DateTime", 19);
+            var source = new byte[] { 50, 48, 50, 49, 45, 48, 49, 45, 48, 50, 32, 48, 51, 58, 48, 52, 58, 48, 53, 0 };
+            var expected = new DateTime(2021, 1, 2,3,4,5);
+            
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(20, record.FixedSize);
+            Assert.Equal(expected, record.ExtractDateFrom(0, source));
+        }
+                
+        [Fact]
+        public void TestReadBoolRecord()
+        {
+            var record = LoadRecordWithValueColumn("Bool", 1);
+            var source = new byte[] { 1 };
+            
+            Assert.Equal(YxdbField.DataType.Boolean, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(1, record.FixedSize);
+            Assert.Equal(true, record.ExtractBoolFrom(0, source));
+            Assert.Equal(true, record.ExtractBoolFrom("value", source));
+        }
+                
+        [Fact]
+        public void TestReadByteRecord()
+        {
+            var record = LoadRecordWithValueColumn("Byte", 1);
+            var source = new byte[] { 23, 0 };
+            
+            Assert.Equal(YxdbField.DataType.Byte, record.Fields[0].Type);
+            Assert.Equal(false, record.HasVar);
+            Assert.Equal(2, record.FixedSize);
+            Assert.Equal((byte)23, record.ExtractByteFrom(0, source));
+            Assert.Equal((byte)23, record.ExtractByteFrom("value", source));
+        }
+                
+        [Fact]
+        public void TestReadBlobRecord()
+        {
+            var record = LoadRecordWithValueColumn("Blob", 100);
+            var source = new byte[] { 0, 0, 0, 0, 4, 0, 0, 0, 1,2,3,4,5,6,7,8 };
+            
+            Assert.Equal(YxdbField.DataType.Blob, record.Fields[0].Type);
+            Assert.Equal(true, record.HasVar);
+            Assert.Equal(4, record.FixedSize);
+            Assert.Equal(new byte[]{}, record.ExtractBlobFrom(0, source));
+            Assert.Equal(new byte[]{}, record.ExtractBlobFrom("value", source));
+        }
+                
+        [Fact]
+        public void TestReadSpatialObjRecord()
+        {
+            var record = LoadRecordWithValueColumn("SpatialObj", 100);
+            var source = new byte[] { 0, 0, 0, 0, 4, 0, 0, 0, 1,2,3,4,5,6,7,8 };
+            
+            Assert.Equal(true, record.HasVar);
+            Assert.Equal(4, record.FixedSize);
+            Assert.Equal(new byte[]{}, record.ExtractBlobFrom(0, source));
+        }
+
+        private static YxdbRecord LoadRecordWithValueColumn(string type, int size)
+        {
+            var fields = new List<MetaInfoField>
+            {
+                new MetaInfoField("value", type, size, 0)
+            };
+            return YxdbRecord.FromFieldList(fields);
         }
     }
 }
